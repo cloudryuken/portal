ecf70307c4662c339d313e44072c47c7d4ba0927
Ark <ark@eclipse.code>
2017-03-30 21:42:20 +0300

    Start View

--- portal/src/portal/Admin_ViewJob.java
+++ portal/src/portal/Admin_ViewJob.java
@@ -1,6 +1,8 @@
 package portal;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,6 +10,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Admin_ViewJob extends JFrame {
 
@@ -16,6 +20,7 @@ public class Admin_ViewJob extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -39,11 +44,14 @@ public class Admin_ViewJob extends JFrame {
 	public Admin_ViewJob() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 320, 200);
+		setBounds(100, 100, 506, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
 		
 		JButton btnNewButton = new JButton("BACK");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -52,8 +60,31 @@ public class Admin_ViewJob extends JFrame {
 				new AdminMain().setVisible(true);
 			}
 		});
-		btnNewButton.setBounds(107, 63, 89, 23);
+		btnNewButton.setBounds(10, 487, 89, 23);
 		contentPane.add(btnNewButton);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"User", "Contractor", "Job Type", "City", "Street", "Date", "Hour Start", "Hour End"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Object.class, String.class, String.class, String.class, Object.class, String.class, String.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table.setBounds(10, 11, 417, 370);
+		contentPane.add(table);
 	}
-
 }
